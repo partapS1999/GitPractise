0afa2ca1be8900ec1abbc0104a131c28c5bd8bc0
DELL <partapS1999>
2022-02-04 16:43:37 +0530

    Added Two lines

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,6 +3,9 @@ public class GitTest {
     public  static void main(String arg[]){
 
         System.out.println("HLOOOO****");
+        System.out.println("HLOOOO****1");
+        System.out.println("HLOOOO****2");
+
     }
 
 }
